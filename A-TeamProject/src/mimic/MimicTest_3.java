f575bacae4cb0154b1ad5fee667ec9f49e8305ac
Twik323 <38524007+Twik323@users.noreply.github.com>
2018-04-30 09:39:13 +0200

    Updates 09:38 selenium testfall Nancy

--- A-TeamProject/src/mimic/MimicTest_3.java
+++ A-TeamProject/src/mimic/MimicTest_3.java
@@ -7,6 +7,16 @@ import org.junit.Test;
 
 public class MimicTest_3 {
 	
+	public void testUnlearn() {
+		Mimic mimic = new Mimic();
+		mimic.goToPage("http://localhost:8080/Number");
+		mimic.learn("One");
+		mimic.unlearn();
+		mimic.goToPage("http://localhost:8080/Number");
+		assertTrue(mimic.checkUnlearn("Paste or type")); //ej pass
+		mimic.closeBrowser();
+	}
+	
 	public void testPriceDollars() {
 		Mimic mimic = new Mimic();
 		mimic.goToPage("http://localhost:8080/LearnNextResponse?text=100$");
@@ -30,7 +40,7 @@ public class MimicTest_3 {
 		mimic.goToPage("http://localhost:8080/LearnNextResponse?text=50%");
 		mimic.learn("Percentage");
 		mimic.goToPage("http://localhost:8080/Percentage");
-		assertTrue(mimic.checkResponse("50%")); //skall vara rött
+		assertTrue(mimic.checkResponse("50%")); //Det blir rött, den skriver "Provide text" i formuläret
 		mimic.closeBrowser();
 	}
 	
@@ -40,13 +50,13 @@ public class MimicTest_3 {
 		mimic.goToPage("http://localhost:8080/LearnNextResponse?text=%50");
 		mimic.learn("Percentage");
 		mimic.goToPage("http://localhost:8080/Percentage");
-		assertTrue(mimic.checkResponse("%50")); //skall vara rött
+		assertTrue(mimic.checkResponse("%50")); //Det blir rött, den skriver "P" i formuläret
 		mimic.closeBrowser();
 	}
 
 
 	@Test
-	public void testOrder1() { testPriceDollars(); testPriceSek(); testPercentage(); testPercentage2();}
+	public void testOrder1() { testUnlearn(); testPriceDollars(); testPriceSek(); testPercentage(); testPercentage2();}
 
 }
 
